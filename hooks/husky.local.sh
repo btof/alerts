ec76170ed17374101d3e9920ecd00ade764576e9
btof <bt12of@gmail.com>
2020-10-20 15:39:03 +0300

    add husky, prettier and eslint

---
+++ hooks/husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.3.0 (https://github.com/typicode/husky#readme)
+#   At: 10/20/2020, 11:41:19 AM
+#   From: undefined (https://github.com/typicode/husky#readme)
+
+packageManager=npm
+cd "."
