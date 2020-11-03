6b1089b278fe490b8431fd5439c271f041fdc2ce
btof <bt12of@gmail.com>
2020-11-03 15:15:59 +0200

    add basic test using mocha and chai

---
+++ src/tests/try.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('Options tests', () => { // the tests container
+    it('checking default options', () => { // the single test
+        expect(3).to.equal(3);
+    });
+});
